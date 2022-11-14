2e0c09af2ea0123f2f6b54c140594e6b83a3c1b5
Neel <njoshi4433@gmail.com>
2022-11-14 19:12:02 +0530

    crate event and functions filess

---
+++ functions/script.js
@@ -0,0 +1,14 @@
+// JS Functions
+
+
+function addition() {
+  let f = parseInt(document.getElementById("firstinput").value);
+  let s = parseInt(document.getElementById("secondinput").value);
+  let result = f + s;
+
+ return document.getElementById('result').innerHTML = result;
+}
+
+// let x = add(n, j)
+
+// document.getElementById('function').innerHTML = x;
